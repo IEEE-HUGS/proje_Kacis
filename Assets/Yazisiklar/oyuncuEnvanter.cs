d2047db41362100b48c24c7e62a15c6b39f438ce
[HQ] IEEE HUGS <ieeehugs@gmail.com>
2020-04-04 19:29:10 +0300

    v0.1a
    - Temel harita spriteları
    - Temel çarpışmalar
    eklendi.
    
    Co-Authored-By: zkngkdnz <zkngkdnz@users.noreply.github.com>

---
+++ Assets/Yazisiklar/oyuncuEnvanter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oyuncuEnvanter : MonoBehaviour
+{
+
+    public GameObject[] envanter = new GameObject[0];
+
+    public void EsyaEkle(GameObject esya)
+    {
+
+        bool esyaEklendi = false;
+
+        for (int i=0; i < envanter.Length; i++) //envanterdeki ilk boş slotu bul
+        {
+            if(envanter[i] == null)
+            {
+                envanter[i] = esya;
+                Debug.Log(esya.name + " eklendi.");
+                esyaEklendi = true;
+                esya.SendMessage("EtkilesimdeBulun");
+                break;
+            }
+        }
+
+        if (!esyaEklendi) //envanter dolu
+        {
+            Debug.Log("Envanter dolu. Eşya eklenemedi.");
+
+        }
+
+    }
+
+}
---
+++ Assets/Yazisiklar/oyuncuHareket.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OyuncuHareket : MonoBehaviour
+{
+
+    public float hareketHizi = 5f;
+
+    public Rigidbody2D kc;
+
+    public Animator animator;
+
+
+    Vector2 hareket;
+  
+    void Update() //Girdiler
+    {
+
+        hareket.x = Input.GetAxisRaw("Horizontal");
+        hareket.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Yatay", hareket.x);
+        animator.SetFloat("Dikey", hareket.y);
+        animator.SetFloat("Hız", hareket.sqrMagnitude);
+
+    }
+
+    void FixedUpdate() //Hareket kodları 
+    {
+
+        kc.MovePosition(kc.position + hareket * hareketHizi * Time.fixedDeltaTime);
+        
+
+    }
+}
