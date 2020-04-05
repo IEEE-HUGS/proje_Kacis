5c1e8a547946b0c6328357f554dd16a9a44f30d5
[HQ] IEEE HUGS <ieeehugs@gmail.com>
2020-04-05 12:19:10 +0300

    v0.2a1
    - Temel envanter mekaniklerinde geliştirmelere gidildi (envanter sıfırlama eklendi)
    - Temel etkileşim mekanikleri geliştirildi (Oyuncu envanterindeki bir eşya ile kilitli objeleri açabilir hâle getirildi)
    - Genel kod düzenlemeleri, iyileştirmeler, anlaşılabilirliği artırma çalışmaları
    yapıldı.
    
    Co-Authored-By: zkngkdnz <zkngkdnz@users.noreply.github.com>

---
+++ Assets/Yazisiklar/esyaEtkilesim.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class esyaEtkilesim : MonoBehaviour
+{
+
+    public bool envantereAlinabilir;       //eğer "doğru" ise obje envanterde tutulabilir.
+
+    public void EtkilesimdeBulun()
+    {
+        gameObject.SetActive(false); //eşyayı görünmez kıl
+    }
+
+}
--- Assets/Yazisiklar/oyuncuEnvanter.cs
+++ Assets/Yazisiklar/oyuncuEnvanter.cs
@@ -9,6 +9,7 @@ public class oyuncuEnvanter : MonoBehaviour
 
     public void EsyaEkle(GameObject esya)
     {
+
         bool esyaEklendi = false;
 
         for (int i=0; i < envanter.Length; i++) //envanterdeki ilk boş slotu bul
@@ -26,26 +27,9 @@ public class oyuncuEnvanter : MonoBehaviour
         if (!esyaEklendi) //envanter dolu
         {
             Debug.Log("Envanter dolu. Eşya eklenemedi.");
+
         }
+
     }
 
-    public void EsyaSil()
-    {
-        envanter = new GameObject[1];
-        Debug.Log("Eşya silindi");
-    }
-
-    public bool EsyaBul(GameObject esya)
-    {
-        for (int i = 0; i < envanter.Length; i++)
-        {
-            if(envanter[i] == esya)
-            {
-                //eşya bulundu
-                return true;
-            }
-        }
-        //eşya bulunamadı  
-        return false;
-    }
 }
