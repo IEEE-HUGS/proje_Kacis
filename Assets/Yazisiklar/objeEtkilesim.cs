8105b7fd24ee95e17295f91df802c041cdc39507
[HQ] IEEE HUGS <ieeehugs@gmail.com>
2020-04-05 14:03:41 +0300

    Revert "v0.2a1"
    This reverts commit 5c1e8a547946b0c6328357f554dd16a9a44f30d5.

---
+++ Assets/Yazisiklar/objeEtkilesim.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objeEtkilesim : MonoBehaviour
+{
+    public bool envantereAlinabilir;        //eğer "doğru" ise obje envanterde tutulabilir.
+    public bool acilabilir;                 //eğer "doğru" ise obje açılabilir.
+    public bool kilitli;                    //eğer "doğru" ise obje kilitlidir ve etkileşime geçilemez.
+    public GameObject gerekliEsya;          //etkileşim için gerekli eşya
+
+    public Animator anim;
+
+    public void EtkilesimdeBulun()
+    {
+        gameObject.SetActive(false); //eşyayı görünmez kıl
+    }
+
+    public void Ac()
+    {
+        anim.SetBool("acik", true);
+    }
+}
